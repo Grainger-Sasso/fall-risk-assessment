381e4ccb384ff0c414d50d768b9f2a458246bd1e
Grainger Sasso <graingersasso@Graingers-MacBook-Air.local>
2025-01-27 10:44:29 -0800

    added dataclass decorator to several classes

--- src/data_model/data/imu/imu_metadata.py
+++ src/data_model/data/imu/imu_metadata.py
@@ -7,6 +7,7 @@ from src.util.mechanics.coordinates.system.anatomical.anatomical_axis import (
 from src.util.mechanics.coordinates.system.sensor.sensor_axis import SensorAxis
 
 
+@dataclass
 class IMUMetadata:
     imu_identifier: IMUIdentifier
     imu_orientation_map: dict[SensorAxis:AnatomicalAxis]
