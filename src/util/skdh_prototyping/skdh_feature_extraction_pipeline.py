5d905d5991b549f71c74d46ae8567c41c5ba3063
Grainger Sasso <graingersasso@Graingers-MacBook-Air.local>
2025-03-24 11:19:08 -0400

    Adding feature engineering to the skdh gait feature extraction

--- src/util/skdh_prototyping/skdh_feature_extraction_pipeline.py
+++ src/util/skdh_prototyping/skdh_feature_extraction_pipeline.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import skdh
-from skdh.gait.gait_metrics import gait_metrics
 from pygt3x.reader import FileReader
+from skdh.gait.gait_metrics import gait_metrics
 
 from src.data_io.builders.file_builders.data.imu.imu_data_file_builder import (
     IMUDataFileBuilder,
@@ -32,7 +32,6 @@ from src.identifiers.user.user_identifier import UserIdentifier
 from src.util.mechanics.coordinates.system.anatomical.anatomical_axis import (
     AnatomicalAxis,
 )
-from src.util.skdh_prototyping.output_gait_feature_keys import OutputGaitFeatureKeys
 from src.util.mechanics.coordinates.system.anatomical.anatomical_coordinate_system import (
     AnatomicalCoordinateSystem,
 )
@@ -40,6 +39,8 @@ from src.util.mechanics.coordinates.system.sensor.sensor_axis import SensorAxis
 from src.util.mechanics.coordinates.system.sensor.sensor_coordinate_system import (
     SensorCoordinateSystem,
 )
+from src.util.skdh_prototyping.gait_feature_keys import GaitFeatureKeys
+from src.util.skdh_prototyping.output_gait_feature_keys import OutputGaitFeatureKeys
 
 
 class SKDHFeatureExtractionPipeline:
@@ -48,13 +49,64 @@ class SKDHFeatureExtractionPipeline:
         self.model_builder = IMUDataBuilder()
         self.file_writer = HDF5FileWriter()
         self.file_reader = HDF5FileReader()
+        self.gait_res_key = "GaitLumbar"
+        self.event_gait_metrics: List[GaitFeatureKeys] = [
+            GaitFeatureKeys.STRIDE_TIME,
+            GaitFeatureKeys.STRIDE_TIME_ASYMMETRY,
+            GaitFeatureKeys.STANCE_TIME,
+            GaitFeatureKeys.STANCE_TIME_ASYMMETRY,
+            GaitFeatureKeys.SWING_TIME,
+            GaitFeatureKeys.SWING_TIME_ASYMMETRY,
+            GaitFeatureKeys.STEP_TIME,
+            GaitFeatureKeys.STEP_TIME_ASYMMETRY,
+            GaitFeatureKeys.INITIAL_DOUBLE_SUPPORT,
+            GaitFeatureKeys.INITIAL_DOUBLE_SUPPORT_ASYMMETRY,
+            GaitFeatureKeys.TERMINAL_DOUBLE_SUPPORT,
+            GaitFeatureKeys.TERMINAL_DOUBLE_SUPPORT_ASYMMETRY,
+            GaitFeatureKeys.DOUBLE_SUPPORT,
+            GaitFeatureKeys.DOUBLE_SUPPORT_ASYMMETRY,
+            GaitFeatureKeys.SINGLE_SUPPORT,
+            GaitFeatureKeys.SINGLE_SUPPORT_ASYMMETRY,
+            GaitFeatureKeys.M2_DELTA_H,
+            GaitFeatureKeys.M2_DELTA_H_PRIME,
+            GaitFeatureKeys.STEP_LENGTH,
+            GaitFeatureKeys.STEP_LENGTH_ASYMMETRY,
+            GaitFeatureKeys.STRIDE_LENGTH,
+            GaitFeatureKeys.STRIDE_LENGTH_ASYMMETRY,
+            GaitFeatureKeys.GAIT_SPEED,
+            GaitFeatureKeys.GAIT_SPEED_ASYMMETRY,
+            GaitFeatureKeys.CADENCE,
+            GaitFeatureKeys.M1_DELTA_H,
+            GaitFeatureKeys.STEP_LENGTH_M1,
+            GaitFeatureKeys.STEP_LENGTH_M1_ASYMMETRY,
+            GaitFeatureKeys.STRIDE_LENGTH_M1,
+            GaitFeatureKeys.STRIDE_LENGTH_M1_ASYMMETRY,
+            GaitFeatureKeys.GAIT_SPEED_M1,
+            GaitFeatureKeys.GAIT_SPEED_M1_ASYMMETRY,
+            GaitFeatureKeys.INTRA_STEP_COVARIANCE_V,
+            GaitFeatureKeys.INTRA_STRIDE_COVARIANCE_V,
+            GaitFeatureKeys.HARMONIC_RATIO_V,
+            GaitFeatureKeys.STRIDE_SPARC,
+        ]
+        self.bout_gait_metrics: List[GaitFeatureKeys] = [
+            GaitFeatureKeys.BOUT_DURATION,
+            GaitFeatureKeys.BOUT_STEPS,
+            GaitFeatureKeys.GAIT_CYCLES,
+            GaitFeatureKeys.DEBUG_MEAN_STEP_FREQ,
+            GaitFeatureKeys.BOUT_PHASE_COORDINATION_INDEX,
+            GaitFeatureKeys.BOUT_GAIT_SYMMETRY_INDEX,
+            GaitFeatureKeys.BOUT_STEP_REGULARITY_V,
+            GaitFeatureKeys.BOUT_STRIDE_REGULARITY_V,
+            GaitFeatureKeys.BOUT_AUTOCOVARIANCE_SYMMETRY_V,
+            GaitFeatureKeys.BOUT_REGULARITY_INDEX_V,
+        ]
 
     def extract_metrics(
         self, input_file_path: Path, output_file_path: Optional[Path] = None
     ) -> Path:
         h5_file: HDF5Group = self.file_reader.read(input_file_path)
         imu_data: IMUData = self.model_builder.build(h5_file)
-        del(h5_file)
+        del h5_file
         pipeline: skdh.Pipeline = self._build_pipeline(output_file_path)
         sensor_data: SensorData = imu_data.data[0].data[0]
         time = sensor_data.time
@@ -68,9 +120,66 @@ class SKDHFeatureExtractionPipeline:
             )
         )
         res = pipeline.run(time=time, accel=accel, height=1.52)
-        gait_res: Dict = res['GaitLumbar']
+        gait_res: Dict = res[self.gait_res_key]
+        self.aggregate_multi_day_metrics(gait_res)
 
-        
+    def aggregate_multi_day_metrics(
+        self, gait_res: Dict
+    ) -> List[Dict[GaitFeatureKeys, float]]:
+        # Init results dictionary (dayN - boutN)
+        multi_day_metrics: List[Dict[GaitFeatureKeys, float]] = []
+        # Initialize pointers for to traverse days and bouts
+        day_start_ix = 0
+        day_n = 1
+        # Reference bout numbers and days from resutls
+        day_n_list = gait_res[GaitFeatureKeys.DAY_N.value]
+        # Traverse days
+        while day_start_ix < len(day_n_list):
+            day_end_ix = day_start_ix
+            # Traverse days to find end day index
+            while day_n_list[day_end_ix] == day_n and day_end_ix < len(day_n_list):
+                day_end_ix += 1
+            # Aggregate bouts, add to result
+            multi_day_metrics.extend(
+                self.aggregate_single_day_metrics(gait_res, day_start_ix, day_end_ix)
+            )
+            # Increment day_n and day_start_ix
+            day_n += 1
+            day_start_ix = day_end_ix
+        # Return results
+        return multi_day_metrics
+
+    def aggregate_single_day_metrics(
+        self, gait_res, day_start_ix: int, day_end_ix: int
+    ) -> List[Dict[GaitFeatureKeys, float]]:
+        single_day_metrics = []
+        bout_n_list = gait_res[GaitFeatureKeys.BOUT_N.value]
+        bout_start_ix = day_start_ix
+        bout_n = 1
+        while bout_start_ix < len(bout_n_list):
+            bout_metrics = {}
+            bout_end_ix = bout_start_ix
+            while bout_n_list[bout_end_ix] == bout_n and bout_end_ix < len(bout_n_list):
+                bout_end_ix += 1
+            for event_metric in self.event_gait_metrics:
+                bout_metrics[event_metric] = np.mean(
+                    gait_res[event_metric.value][bout_start_ix:bout_end_ix]
+                )
+            for bout_metric in self.bout_gait_metrics:
+                bout_metrics[bout_metric] = gait_res[bout_metric.value][bout_start_ix]
+            single_day_metrics.append(bout_metrics)
+            bout_n += 1
+            bout_start_ix = bout_end_ix
+        return single_day_metrics
+
+    def aggregate_bout_level_metrics(self):
+        # Define the specific metrics in the gait results which are to be aggregated
+        # Define the descriptive statistics to be used in aggregating metrics
+        # For each one of those gait metrics and descriptive stats, generate aggregate data objects
+        pass
+
+    def export_aggregate_metrics(self):
+        pass
 
     def _build_pipeline(self, output_file_path: Optional[Path]) -> skdh.Pipeline:
         """Extracts features
