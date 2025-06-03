9daaaff5e5854a872dd4817534abd56de72e6314
Grainger Sasso <graingersasso@Graingers-Air.lan>
2025-06-03 12:23:35 -0400

    Finished converter for DAT to H5, need to run this for all users found in LTMM

--- src/util/data_converters/dat/dat_to_h5_converter.py
+++ src/util/data_converters/dat/dat_to_h5_converter.py
@@ -1,20 +1,56 @@
 import time
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 import numpy as np
 import wfdb
 
+from src.data_io.builders.file_builders.data.imu.imu_data_file_builder import (
+    IMUDataFileBuilder,
+)
+from src.data_io.builders.model_builders.data.imu.imu_data_builder import IMUDataBuilder
+from src.data_io.formats.hdf5.hdf5_group import HDF5Group
+from src.data_io.model_fields.data.imu.imu_data_fields import IMUDataFields
+from src.data_io.read_write.readers.hdf5.hdf5_file_reader import HDF5FileReader
+from src.data_io.read_write.writers.hdf5.hdf5_file_writer import HDF5FileWriter
+from src.data_model.data.imu.epoch_imu_data import EpochIMUData
+from src.data_model.data.imu.imu_data import IMUData
+from src.data_model.data.imu.metadata.imu_metadata import IMUMetadata
+from src.data_model.data.imu.metadata.sensor_metadata import SensorMetadata
+from src.data_model.data.imu.sensor_data import SensorData
+from src.data_model.data.imu.uniaxial_sensor_data import UniaxialSensorData
+from src.data_types.instrument.sensor_type import SensorType
+from src.id_generator.imu.imu_data_identifier_generator import (
+    IMUDataIdentifierGenerator,
+)
+from src.identifiers.imu.imu_data_identifier import IMUDataIdentifier
+from src.identifiers.instrument.instrument_identifier import (
+    InstrumentIdentifier,
+)
+from src.identifiers.user.user_identifier import UserIdentifier
+from src.util.mechanics.coordinates.system.anatomical.anatomical_axis import (
+    AnatomicalAxis,
+)
 from src.util.mechanics.coordinates.system.anatomical.anatomical_coordinate_system import (
     AnatomicalCoordinateSystem,
 )
-from src.data_io.model_fields.data.imu.imu_data_fields import IMUDataFields
+from src.util.mechanics.coordinates.system.sensor.sensor_axis import SensorAxis
+from src.util.mechanics.coordinates.system.sensor.sensor_coordinate_system import (
+    SensorCoordinateSystem,
+)
 
 
 class DATToHDF5Converter:
 
     SAMPLING_RATE = 100.0
     """Converter for DAT files to HDF5 format."""
+
+    def __init__(self):
+        self.imu_id_gen = IMUDataIdentifierGenerator()
+        self.file_builder = IMUDataFileBuilder()
+        self.model_builder = IMUDataBuilder()
+        self.file_writer = HDF5FileWriter()
+        self.file_reader = HDF5FileReader()
 
     def read_dat_file(self, input_file_path: Path) -> Optional[np.ndarray]:
         """Read data from a .dat file.
@@ -44,7 +80,7 @@ class DATToHDF5Converter:
             print(f"Error reading .dat file: {str(e)}")
             return None
 
-    def read_dat_record_wfdb(self, path: Path):
+    def read_dat_record_wfdb(self, path: Path) -> Dict[Any, Any]:
         """Reads and parses data records for LTMM dataset
 
         Args:
@@ -79,14 +115,87 @@ class DATToHDF5Converter:
         data[IMUDataFields.TIME] = time_axis
         return data
 
+    def convert_to_imu_data(self, data, user_id: str) -> IMUData:
+        epoch_imu_data_list: List[EpochIMUData] = self._build_epoch_data(data)
+        imu_data_id: IMUDataIdentifier = self.imu_id_gen.generate_identifier()
+        user_data_id: UserIdentifier = UserIdentifier(user_id)
+        inst_id: InstrumentIdentifier = InstrumentIdentifier("placeholder", "001")
+        metadata: IMUMetadata = IMUMetadata(
+            imu_data_identifier=imu_data_id,
+            user_identifier=user_data_id,
+            instrument_identifier=inst_id,
+        )
+        start_time = data[IMUDataFields.TIME][0]
+        end_time = data[IMUDataFields.TIME][-1]
+        return IMUData(
+            data=epoch_imu_data_list,
+            metadata=metadata,
+            start_time=start_time,
+            end_time=end_time,
+        )
+
+    def export_imu_data_to_h5(self, data: IMUData, output_path: Path):
+        group: HDF5Group = self.file_builder.build(data)
+        self.file_writer.write(output_path, group)
+        return
+
+    def _build_epoch_data(self, data) -> List[EpochIMUData]:
+        sensor_data = self._build_sensor_data(data)
+        start_time = data[IMUDataFields.TIME][0]
+        end_time = data[IMUDataFields.TIME][-1]
+        return [
+            EpochIMUData(
+                data=[sensor_data], epoch_start_time=start_time, epoch_end_time=end_time
+            )
+        ]
+
+    def _build_sensor_data(self, data) -> List[SensorData]:
+        x_axis = UniaxialSensorData(
+            anatomical_axis=AnatomicalAxis(AnatomicalCoordinateSystem.VERTICAL),
+            sensor_axis=SensorAxis(SensorCoordinateSystem.X),
+            data=data[AnatomicalCoordinateSystem.VERTICAL],
+        )
+        y_axis = UniaxialSensorData(
+            anatomical_axis=AnatomicalAxis(AnatomicalCoordinateSystem.MEDIOLATERAL),
+            sensor_axis=SensorAxis(SensorCoordinateSystem.Y),
+            data=data[AnatomicalCoordinateSystem.MEDIOLATERAL],
+        )
+        z_axis = UniaxialSensorData(
+            anatomical_axis=AnatomicalAxis(AnatomicalCoordinateSystem.ANTEROPOSTERIOR),
+            sensor_axis=SensorAxis(SensorCoordinateSystem.Z),
+            data=data[AnatomicalCoordinateSystem.ANTEROPOSTERIOR],
+        )
+        sensor_metadata = SensorMetadata(
+            sensor_type=SensorType.ACCELEROMETER,
+            sampling_rate=DATToHDF5Converter.SAMPLING_RATE,
+            unit="g",
+        )
+        time = data[IMUDataFields.TIME]
+        idle_mask = np.array([0 for _ in range(len(data[AnatomicalCoordinateSystem.ANTEROPOSTERIOR]))])
+        return SensorData(
+            data=[x_axis, y_axis, z_axis],
+            time=time,
+            idle_mask=idle_mask,
+            metadata=sensor_metadata,
+        )
+
+    def test_read_converted_file(self, path: Path):
+        h5_file: HDF5Group = self.file_reader.read(path)
+        return self.model_builder.build(h5_file)
+
 
 def main():
     path = Path(
         "/Users/graingersasso/Desktop/fafra_data/raw_data/ltmm/long-term-movement-monitoring-database-1.0.0/CO001"
     )
+    output_path = Path(
+        "/Users/graingersasso/Desktop/fafra_data/raw_data/ltmm_h5/test.h5"
+    )
     converter = DATToHDF5Converter()
     data = converter.read_dat_record_wfdb(path)
-    print("f")
+    imu_data = converter.convert_to_imu_data(data, "dummy_user_id")
+    converter.export_imu_data_to_h5(imu_data, output_path)
+    converted_imu_data = converter.test_read_converted_file(output_path)
 
 
 if __name__ == "__main__":
